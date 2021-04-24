81aad82dd1ccf449a5828ca11ae4630b2b6b3830
Developer <dev@example.com>
2021-04-24 16:43:24 +0800

    feat: add migration state variables and events for legacy assets

--- contracts/GameItem.sol
+++ contracts/GameItem.sol
@@ -20,11 +20,19 @@ contract GameItem {
     mapping (uint256 => address) public creators;
     mapping (bytes4 => bool) private _supportedInterfaces;
     mapping (address => uint256) private _balances;
+
+    bytes32 public migrationRoot;
+    uint256 public totalMigrated;
+    mapping (bytes32 => bool) private _migrationClaims;
+    mapping (uint256 => uint256) public legacyToToken;
+
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     event Approval(address indexed _owner, address indexed approved, uint256 indexed tokenId);
     event ApprovalForAll(address indexed _owner, address indexed operator, bool approved);
     event Mint(address indexed to, uint256 indexed tokenId, string uri);
     event Burn(address indexed from, uint256 indexed tokenId);
+    event Migrated(address indexed user, uint256 indexed legacyItemId, uint256 indexed newTokenId, string uri);
+    event MigrationRootUpdated(bytes32 oldRoot, bytes32 newRoot);
     bytes4 constant ERC165_INTERFACE_ID = bytes4(0x01ffc9a7);
     bytes4 constant ERC721_INTERFACE_ID = bytes4(0x80ac58cd);
     bytes4 constant ERC721_RECEIVED = bytes4(0x150b7a02);
